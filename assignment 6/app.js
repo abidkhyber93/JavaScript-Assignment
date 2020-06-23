7c7e972dc03a0041f736d922dd0c81649e2409e5
abidkhyber93 <57323216+abidkhyber93@users.noreply.github.com>
2020-06-23 10:07:18 +0500

    Update app.js

--- assignment 6/app.js
+++ assignment 6/app.js
@@ -99,3 +99,13 @@
 // Whether the user enters cookie, Cookie, COOKIE or coOkIE, program should inform about its availability. Example:
 
 // var A = [cake”, “apple pie”, “cookie”, “chips”, “patties”]
+
+
+// 15. Write a program to take password as an input from user.
+// The password must qualify these requirements:
+// a. It should contain alphabets and numbers 
+// b. It should not start with a number 
+// c. It must at least 6 characters long If the password does not meet above requirements, 
+//   prompt the user to enter a valid password. For character codes of a-z, A-Z & 0-9, refer to ASCII table at the end of this document.
+
+
